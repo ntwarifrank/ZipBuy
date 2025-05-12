57d7a7ce80b53f5c600e93351cfe183b2edd4463
ntwarifrank <ntwarifrank100@gmail.com>
2025-05-12 17:46:30 +0200

    Fix Tailwind CSS PostCSS plugin issue

--- Zipbuy_Admin/postcss.config.mjs
+++ Zipbuy_Admin/postcss.config.mjs
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+// postcss.config.mjs
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
